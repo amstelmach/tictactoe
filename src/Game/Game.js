63b9277636121b962083d5646d7d85286b047a7a
Alicja Stelmach <alicjaastelmach@gmail.com>
2022-01-18 14:19:09 +0100

    handleClick check

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -2,8 +2,16 @@ import React, { useState } from 'react';
 import './Game.css';
 
 const TicTacToe = () => {
-  const Cell = () => {
-    return <div className='cell'></div>;
+  const handleClick = (num) => {
+    alert(num);
+  };
+
+  const Cell = ({ num }) => {
+    return (
+      <div onClick={() => handleClick(num)} className='cell'>
+        -
+      </div>
+    );
   };
 
   return (
@@ -11,15 +19,15 @@ const TicTacToe = () => {
       <h1>Tic Tac Toe</h1>
 
       <div className='boardgame'>
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell num={0} />
+        <Cell num={1} />
+        <Cell num={2} />
+        <Cell num={3} />
+        <Cell num={4} />
+        <Cell num={5} />
+        <Cell num={6} />
+        <Cell num={7} />
+        <Cell num={8} />
       </div>
     </div>
   );
